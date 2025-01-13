dee9e50f81fd40c10750f239eb7e1345559b27c3
Swati <swatikadamp26@gmail.com>
2025-01-13 10:52:38 +0530

    php info

---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+$array1=[1,2,3,4];
+$array2 = ['a','b','c','d'];
+
+$combine_array = [];
+
+foreach($array1 as $value)
+{
+    $combine_array[] = $value;
+}
+foreach($array2 as $value1)
+{
+    $combine_array[]=$value1;
+}
+
+print_r($combine_array);
+
+
+
+?>
